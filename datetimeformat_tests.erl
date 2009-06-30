1a3cb4c94aededae0643674f700f2b5047d1e606
unknown <rameshr@.corporate.thoughtworks.com>
2009-06-30 16:57:14 +0530

    Ramesh - renamed test methods

--- datetimeformat_tests.erl
+++ datetimeformat_tests.erl
@@ -1,10 +1,10 @@
 -module (datetimeformat_tests).
 -include_lib("/Program Files/erl5.7.1/lib/eunit-2.1/include/eunit.hrl").
 
-formatdatetimevalid_test() ->
+format_datetime_valid_test() ->
 	?assert("2009-6-30 15:25:23" =:= datetimeformat:format({{2009,6,30},{15,25,23}}))
 .
 
-formatdatetimeinvalid1_test() ->
+format_datetime_invalid1_test() ->
 	?assert(invalid =:= datetimeformat:format({{2009},{10}}))
 .
